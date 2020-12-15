225982e9828369352f26e5a15b058c4d64cc3ba9
mansour <mansour@web-id.fr>
2020-12-15 19:07:56 +0100

    connect Api

--- client/src/pages/NotFound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="h-screen w-screen  flex flex-col justify-center content-center flex-wrap">
-  <img src="../assets/img/404.svg" alt="404">
-    <router-link to="/" class="text-red-400 underline font-black text-2xl mt-10">Retour a l'acceuil</router-link>
-</div>
-
-</template>
