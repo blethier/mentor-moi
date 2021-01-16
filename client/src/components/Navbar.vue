3dc44a6e7ba55fa18aa603f02bf8c0316e7f0d68
mansour <mansour@web-id.fr>
2021-01-16 01:31:59 +0100

    add link in navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -40,6 +40,7 @@
             <router-link to="/mentors" active-class="bg-darkSands" class="  px-3 py-2 rounded-md text-lg font-medium">Liste des mentors</router-link>
             <router-link to="/faq" active-class="bg-darkSands" class="  px-3 py-2 rounded-md text-lg font-medium">F.A.Q</router-link>
             <router-link to="/jobs" active-class="bg-darkSands" class="  px-3 py-2 rounded-md text-lg font-medium">Trouver un poste</router-link>
+            <router-link to="/conseils" active-class="bg-darkSands" class="  px-3 py-2 rounded-md text-lg font-medium">Ressources</router-link>
             <router-link v-if="!isLoggedIn" to="/auth" active-class="bg-darkSands" class="  px-3 py-2 rounded-md text-lg font-medium">Se connecter</router-link>
           </div>
         </div>
@@ -68,7 +69,6 @@
               <div class="px-2 pt-2 pb-3 space-y-1">
             <router-link v-if="!isMentor" to="/register" class="block px-4 py-2 hover:bg-sands  " role="menuitem">Devenir Mentor</router-link>
             <router-link v-if="isMentor" to="/updateMentors" class="block px-4 py-2 hover:bg-sands " role="menuitem">Modifier mon profil</router-link>
-            <router-link v-if="isMentor" to="/jobs" class="block px-4 py-2 hover:bg-sands " role="menuitem">Trouver un poste</router-link>
             <p @click="logout" href="#" class="block px-4 py-2 hover:bg-sands " role="menuitem">Déconnexion</p>
               </div>
            
@@ -89,6 +89,8 @@
       <router-link to="/" class="  block px-3 py-2 rounded-md text-base font-medium">Acceuil</router-link>
       <router-link to="/mentors" class="  block px-3 py-2 rounded-md text-base font-medium">Liste des mentors</router-link>
       <router-link to="/faq" class="  block px-3 py-2 rounded-md text-base font-medium">F.A.Q</router-link>
+       <router-link to="/jobs" class="  block px-3 py-2 rounded-md text-base font-medium">Emploi</router-link>
+        <router-link to="/conseils" class="  block px-3 py-2 rounded-md text-base font-medium">Ressources</router-link>
       <router-link to="/auth" v-if="!isLoggedIn" class="  block px-3 py-2 rounded-md text-base font-medium">Se connecter</router-link>
     </div>
   </div>
